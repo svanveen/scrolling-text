4c85d24a802d2a1c20e72f9174035b57d85b6733
Stephan van Veen <vanveen.stephan@gmail.com>
2019-05-15 18:38:02 +0200

    Move array concatenation to Array.h

--- Array.h
+++ Array.h
@@ -2,18 +2,52 @@
 #define SCROLLING_TEXT_ARRAY_H
 
 #include <stdlib.h>
+#include "IndexSequence.h"
 
-template<typename T, size_t N>
+template <typename T, size_t N>
 struct Array {
-    constexpr const T &operator[](size_t index) const {
+    constexpr const T& operator[](size_t index) const
+    {
         return data[index];
     }
 
-    constexpr size_t size() const {
+    constexpr size_t size() const
+    {
         return N;
     }
 
     T data[N];
 };
+
+
+/// Static concatination of arrays
+template <typename T, size_t N1, size_t... I1, size_t N2, size_t... I2>
+constexpr Array<T, N1 + N2> concatImpl(const Array<T, N1>& arr1,
+                                       const Array<T, N2>& arr2,
+                                       const IndexSequence<I1...>&,
+                                       const IndexSequence<I2...>&)
+{
+    return {arr1[I1]..., arr2[I2]...};
+}
+
+template <typename T, size_t N>
+constexpr Array<T, N> concat(const Array<T, N>& arr)
+{
+    return arr;
+}
+
+template <typename T, size_t N1, size_t N2>
+constexpr Array<T, N1 + N2> concat(const Array<T, N1>& arr1, const Array<T, N2>& arr2)
+{
+    return concatImpl(arr1, arr2, MakeIndexSequence < N1 > {}, MakeIndexSequence < N2 > {});
+}
+
+template <typename T, size_t N1, size_t N2, typename ...Args>
+constexpr Array<T, N1 + N2 * (1 + sizeof...(Args))> concat(const Array<T, N1>& arr1,
+                                                           const Array<T, N2>& arr2,
+                                                           const Args& ...args)
+{
+    return concat(concat(arr1, arr2), args...);;
+}
 
 #endif // SCROLLING_TEXT_ARRAY_H
--- utils.h
+++ utils.h
@@ -7,36 +7,6 @@
 #include "char2bytes.h"
 #include "IndexSequence.h"
 
-/// Static concatination of arrays
-template<typename T, size_t N1, size_t... I1, size_t N2, size_t... I2>
-constexpr Array<T, N1 + N2> concatInternal(const Array<T, N1>& arr1,
-                                           const Array<T, N2>& arr2,
-                                           const IndexSequence<I1...>&,
-                                           const IndexSequence<I2...>&)
-{
-    return {arr1[I1]..., arr2[I2]...};
-}
-
-template<typename T, size_t N>
-constexpr Array<T, N> concat(const Array<T, N>& arr)
-{
-    return arr;
-}
-
-template<typename T, size_t N1, size_t N2>
-constexpr Array<T, N1 + N2> concat(const Array<T, N1>& arr1, const Array<T, N2>& arr2)
-{
-    return concatInternal(arr1, arr2, MakeIndexSequence<N1>{}, MakeIndexSequence<N2>{});
-}
-
-template<typename T, size_t N1, size_t N2, typename ...Args>
-constexpr Array<T, N1 + N2 * (1 + sizeof...(Args))> concat(const Array<T, N1>& arr1,
-                                                           const Array<T, N2>& arr2,
-                                                           const Args &...args) {
-    return concat(concat(arr1, arr2), args...);;
-}
-
-
 /// Resolve led representation for characters
 constexpr int length(const char *str) {
     return *str ? 1 + length(str + 1) : 1;
